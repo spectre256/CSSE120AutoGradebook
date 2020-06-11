e973b8418bb5ead575da5af2132346e023f6be26
grayledw <grayledw@rose-hulman.edu>
2020-06-10 23:43:54 -0400

    Changed defaults, default message CSSE120 Summer Updates

--- src/run_autogradebook.py
+++ src/run_autogradebook.py
@@ -459,7 +459,14 @@ def send_emails(email_parameters, server, missing_assignments, test_run, minimum
             else:
                 email['To'] = assignments_list[0]  # Index 0 is the student email
             email['CC'] = email_parameters['cc emails']
-            assignments = ',   '.join(assignments_list[2:])
+            assignments = """
+               
+            """.join(assignments_list[2:])
+            assignments = """
+            
+            {}
+            
+""".format(assignments)
             message = email_parameters['message']
             message = message.replace('[NAME]', assignments_list[1]).replace('[ASSIGNMENTS]', assignments)
             email.set_content(message)
